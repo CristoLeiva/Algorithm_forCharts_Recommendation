36077978e04d60222101608b3aac11e122a19ef2
AhmadAmayri <ahmada87@gmail.com>
2015-03-20 05:04:37 +0100

    ahmad

---
+++ dev/ChartAdvisor/src/com/chartadvisor/model/Property.java
@@ -0,0 +1,44 @@
+package com.chartadvisor.model;
+
+public class Property {
+	
+	private String propertyName;
+	private String propertyType;
+	
+	public String getPropertyName() {
+		return propertyName;
+	}
+	public void setPropertyName(String propertyName) {
+		this.propertyName = propertyName;
+	}
+	public String getPropertyType() {
+		return propertyType;
+	}
+	public void setPropertyType(String propertyType) {
+		this.propertyType = propertyType;
+	}
+	public Property() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Property(String propertyName, String propertyType) {
+		super();
+		this.propertyName = propertyName;
+		this.propertyType = propertyType;
+	}
+	
+	public boolean equals(Object o){
+		if(!(o instanceof Property))
+			return false;
+		Property p = (Property)o;
+		if(p.getPropertyName().equalsIgnoreCase(propertyName))
+			if(p.getPropertyType().equalsIgnoreCase(propertyType))
+				return true;
+		return false;
+	}
+	
+	public String toString(){
+		return new String(propertyName+":"+propertyType);
+	}
+
+}
