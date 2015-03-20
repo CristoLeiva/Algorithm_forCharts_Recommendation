6db9c5ee99447623d63993a0602d678181827a19
AhmadAmayri <ahmada87@gmail.com>
2015-03-20 04:12:41 +0100

    Ahmad

--- dev/ChartAdvisor/src/com/chartadvisor/model/Allocation.java
+++ dev/ChartAdvisor/src/com/chartadvisor/model/Allocation.java
@@ -28,4 +28,21 @@ public class Allocation {
 		
 	}
 	
+	public boolean equals(Object o){
+		if(!(o instanceof Allocation))
+			return false;
+		else{
+			Allocation alloc = (Allocation)o;
+			for (String s : alloc.leftAllocations){
+				if (!leftAllocations.contains(s))
+						return false;
+			}
+			for (String s : alloc.rightAllocations){
+				if (!rightAllocations.contains(s))
+						return false;
+			}
+			return true;
+		}
+	}
+	
 }
