02cd903096ee12b3fe382b1f72f2d238beec8293
Jorge Ortiz <jorgeortiz@colombia.com>
2015-03-22 19:40:21 +0100

    PropertyCompleteName
    PropertyCompleteName

--- dev/ChartAdvisor/src/com/chartadvisor/model/Property.java
+++ dev/ChartAdvisor/src/com/chartadvisor/model/Property.java
@@ -2,13 +2,22 @@ package com.chartadvisor.model;
 
 public class Property {
 	
+	private String propertyCompleteName;
 	private String propertyName;
 	private String propertyType;
+	
+
+	public String getPropertyCompleteName() {
+		return propertyName;
+	}
 	
 	public String getPropertyName() {
 		return propertyName;
 	}
 	public void setPropertyName(String propertyName) {
+		this.propertyName = propertyName;
+	}
+	public void setPropertyCompleteName(String propertyName) {
 		this.propertyName = propertyName;
 	}
 	public String getPropertyType() {
